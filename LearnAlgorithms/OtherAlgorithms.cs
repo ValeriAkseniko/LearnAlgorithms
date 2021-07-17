25325124d3fe25db4d1f81ed498e5cdeca3a0505
ValeriAkseniko <valeri.akseniko@yandex.ru>
2021-07-17 16:21:42 +0300

    fixfix

--- LearnAlgorithms/OtherAlgorithms.cs
+++ LearnAlgorithms/OtherAlgorithms.cs
@@ -20,26 +20,7 @@ namespace LearnAlgorithms
             }
             return result;
         }
-        //public static int CountDayOff(DateTime start, DateTime end)
-        //{
-        //    int result = 0;
-        //    double days = (end - start).TotalDays;
-        //    List<DateTime> allDays = new List<DateTime>();
-        //    int j = 0;
-        //    for (DateTime i = start; j <= days; j++)
-        //    {
-        //        allDays.Add(i);
-        //        i = i.AddDays(1);
-        //    }
-        //    for (int i = 0; i < allDays.Count; i++)
-        //    {
-        //        if (allDays[i].DayOfWeek == DayOfWeek.Sunday || allDays[i].DayOfWeek == DayOfWeek.Saturday)
-        //        {
-        //            result++;
-        //        }
-        //    }
-        //    return result;
-        //}
+
         public static List<DateTime> ListDayOff(DateTime start, DateTime end)
         {
             List<DateTime> result = new List<DateTime>();
@@ -52,6 +33,7 @@ namespace LearnAlgorithms
             }
             return result;
         }
+
         public static DateTime[] ArrayDayOff(DateTime start, DateTime end)
         {
             int arrayLength = CountDayOff(start, end);
