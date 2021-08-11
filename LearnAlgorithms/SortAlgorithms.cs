8e3d3708497c7dea89c3d0cf15cb8f8fab8a52d3
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-08-11 14:21:23 +0300

    create mix collection

--- LearnAlgorithms/SortAlgorithms.cs
+++ LearnAlgorithms/SortAlgorithms.cs
@@ -8,5 +8,31 @@ namespace LearnAlgorithms
 {
     static class SortAlgorithms
     {
+        public static void Mix(int[] array)
+        {
+            Random rnd = new Random();
+            int maxIndex = array.Length - 1;
+            for (int i = 0; i < rnd.Next(0, 100); i++)
+            {
+                int firstIndex = rnd.Next(0, maxIndex);
+                int secondIndex = rnd.Next(0, maxIndex);
+                int blank = array[firstIndex];
+                array[firstIndex] = array[secondIndex];
+                array[secondIndex] = blank;
+            }
+        }
+        public static void Mix(List<int> list)
+        {
+            Random rnd = new Random();
+            int maxIndex = list.Count;
+            for (int i = 0; i < rnd.Next(0,100); i++)
+            {
+                int firstIndex = rnd.Next(0, maxIndex);
+                int secondIndex = rnd.Next(0, maxIndex);
+                int blank = list[firstIndex];
+                list[firstIndex] = list[secondIndex];
+                list[secondIndex] = blank;
+            }
+        }
     }
 }
