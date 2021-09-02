6f10900779c010436e39c41d5a53f54d7551c604
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-02 18:06:15 +0300

    create Date Time extending methods

---
+++ LearnAlgorithms/DateTimeExtendings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnAlgorithms
+{
+    public static class DateTimeExtendings
+    {
+        public static DateTime AddWeeks(this DateTime date, int value)
+        {
+            DateTime newDate = date.AddDays(value);
+            return newDate;
+        }
+
+        public static DateTime StartWeek(this DateTime date)
+        {
+            int dayValue = (int)date.DayOfWeek;
+            DateTime startWeek = date.AddDays(-1 * dayValue);
+            return startWeek;
+        }
+
+        public static DateTime EndWeek(this DateTime date)
+        {
+            int dayValue = (int)date.DayOfWeek;
+            DateTime endWeek = date.AddDays(7 - dayValue);
+            return endWeek;
+        }
+    }
+}
--- LearnAlgorithms/Program.cs
+++ LearnAlgorithms/Program.cs
@@ -10,13 +10,7 @@ namespace LearnAlgorithms
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            SortAlgorithms.Mix(list);
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
-            Console.ReadKey();
+            
         }
     }
 }
