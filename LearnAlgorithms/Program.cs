8123532a291709826fdedd3aaa5a5bc44897bf56
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-08 17:30:23 +0300

    create quivk sort

--- LearnAlgorithms/Program.cs
+++ LearnAlgorithms/Program.cs
@@ -10,11 +10,8 @@ namespace LearnAlgorithms
     {
         static void Main(string[] args)
         {
-            string text = "я просто не совсем понимаю.можно обратиться к строчке в словаре по значению, а не по ключу ? ";
-
-            Dictionary<string, int> result = new Dictionary<string, int>();
-
-            result = StringAlgorithms.GetWordStatistic(text);
+            int[] array = { 2, 7, 6, 9, 3 };
+            SortAlgorithms.QuickSort(array);
         }
     }
 }
--- LearnAlgorithms/SortAlgorithms.cs
+++ LearnAlgorithms/SortAlgorithms.cs
@@ -8,7 +8,7 @@ namespace LearnAlgorithms
 {
     static class SortAlgorithms
     {
-        public static void Mix(int[] array,int countSwap = 100)
+        public static void Mix(int[] array, int countSwap = 100)
         {
             Random rnd = new Random();
             int maxIndex = array.Length - 1;
@@ -33,6 +33,48 @@ namespace LearnAlgorithms
                 list[firstIndex] = list[secondIndex];
                 list[secondIndex] = blank;
             }
+        }
+
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+
+        //метод возвращающий индекс опорного элемента
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        //быстрая сортировка
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+            return array;
+        }
+
+        public static int[] QuickSort(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
         }
     }
 }
